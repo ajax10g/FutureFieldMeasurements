336fe6fadde3523cd77c156e74cdc840df3df54f
Marcus Kempe <makem86@gmail.com>
2015-10-22 18:32:14 +0200

    Updated install.sh to copy whole folders to /etc/...

--- install.sh
+++ install.sh
@@ -19,31 +19,27 @@ cp $dir/*.conf /etc/init
 #Get modem scripts
 echo "Copying modem scripts to /etc/futurefieldmeasurements."
 dir="modem"
-cp $dir/pingtest.sh /etc/futurefieldmeasurements/$dir
-cp $dir/resetmodem.sh /etc/futurefieldmeasurements/$dir
+cp -r $dir /etc/futurefieldmeasurements
 
 #Get buses
 echo "Copying buses scripts to /etc/futurefieldmeasurements."
 dir="1-wire"
-cp  $dir/main.py /etc/futurefieldmeasurements/$dir
+cp -r  $dir /etc/futurefieldmeasurements
 
 dir="ethercat"
 cp -r  $dir /etc/futurefieldmeasurements
 
 dir="mbus"
-cp  $dir/main.py /etc/futurefieldmeasurements/$dir
+cp -r  $dir /etc/futurefieldmeasurements
 
 dir="modbus-rtu"
-cp  $dir/main.py /etc/futurefieldmeasurements/$dir
+cp -r  $dir /etc/futurefieldmeasurements
 
 dir="modbus-tcp"
-cp  $dir/main.py /etc/futurefieldmeasurements/$dir
+cp -r  $dir /etc/futurefieldmeasurements
 
 dir="dio"
-cp  $dir/main.c /etc/futurefieldmeasurements/$dir
-cp  $dir/main /etc/futurefieldmeasurements/$dir
-cp  $dir/Makefile /etc/futurefieldmeasurements/$dir
-cp -r  $dir/include /etc/futurefieldmeasurements/$dir
+cp -r  $dir /etc/futurefieldmeasurements
 
 echo "Now restarting mosquitto and thus restarting all bus jobs."
 dir="mosquitto"
